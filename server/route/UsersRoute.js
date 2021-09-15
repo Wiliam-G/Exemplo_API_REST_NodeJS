bbae99f7d2b914b46562baa085533aa1713e83c3
unknown <wiliam.gomes99@gmail.com>
2021-09-15 20:37:27 -0300

    Add routes

--- server/route/UsersRoute.js
+++ server/route/UsersRoute.js
@@ -2,24 +2,47 @@ const express = require(express);
 const router = express.router();
 const userServices = require('../service/Services');
 
-router.get('/users', async function(req, res){
-
+router.get('/users', async function(req, res, next){
+    try{
+        const users = await userServices.getUsers();
+        res.json(users);
+    } catch(err){
+        next(err);
+    }
 });
 
 router.get('/users/:id', async function(req, res){
-
+    const user = userServices.getUserById();
+    res.json(user);
 });
 
-router.post('/users', async function(req, res){
-
+router.put('/users/:id', async function(req, res, next){
+    const user = req.body;
+    try{
+        await userService.updateUser(req.params.id, user);
+        res.status(204).end();
+    } catch(err){
+        next(err);
+    }
 });
 
-router.put('/users/:id', async function(req, res){
-
+router.post('/users', async function(req, res, next){
+    const user = req.body;
+    try{
+        newUser = await userServices.createUser(user);
+        res.status(2001).json(newUser);
+    } catch(err){
+        next(err);
+    }
 });
 
-router.delete('/users/:id', async function(req, res){
-
+router.delete('/users/:id', async function(req, res, next){
+    try{
+        await userServices.deleteUser(req.params.id);
+        res.status(204).end();
+    } catch(err){
+        next(err);
+    }
 });
 
 module.exports = router;
